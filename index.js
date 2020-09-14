4e0ef350b7765e81c89056c0f6ad267158165461
David Felix <david.felix02@hotmail.com>
2020-09-14 01:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -25,6 +25,6 @@ function walkHome (dogName, dogBreed) {
 }
 
 function unleashDog (dogName, dogBreed) {
-  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
-  return `Throw the frisbee for ${dogName} the ${dogBreed}`;
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `Unleash ${dogName} the ${dogBreed}`;
 }
