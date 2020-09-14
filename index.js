e062b29adbbfc19b3b360b9707c983cd6b1c25d5
David Felix <david.felix02@hotmail.com>
2020-09-14 01:24:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,5 +33,5 @@ var routine = [wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
 
 function exerciseDog (dogName, dogBreed) {
   // return routine.forEach(dogName,dogBreed);
-  return routine.map(fn => fn(dogName, dogBreed))
+  return routine.map(action => action(dogName, dogBreed))
 }
