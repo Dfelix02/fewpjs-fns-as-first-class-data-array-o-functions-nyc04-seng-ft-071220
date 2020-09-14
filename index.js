4b7756bdfd7f73d6042670ea4ae6c4451292193f
David Felix <david.felix02@hotmail.com>
2020-09-14 01:11:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function leashDog (dogName, dogBreed) {
   console.log(`Leash ${dogName} the ${dogBreed}`);
   return `Leash ${dogName} the ${dogBreed}`;
 }
+
+function wakeDog (dogName, dogBreed) {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`;
+}
